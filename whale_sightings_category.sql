b488dd93fab849f09b8f375b812aba7f321831db
layanakhras <101355989+layanakhras@users.noreply.github.com>
2024-04-22 20:01:31 -0400

    backend

--- whale_sightings_category.sql
+++ whale_sightings_category.sql
@@ -1,41 +1,36 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
---
--- Host: localhost    Database: whale_sightings
--- ------------------------------------------------------
--- Server version	8.0.36
+CREATE DATABASE IF NOT EXISTS whale_sightings;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE whale_sightings;
 
---
--- Table structure for table `category`
---
+CREATE TABLE Sightings (
+    sighting_id INT AUTO_INCREMENT PRIMARY KEY,
+    sighting_date DATE,
+    group_size INT,
+    latitude DECIMAL(10, 6),
+    longitude DECIMAL(10, 6),
+    certainty VARCHAR(50),
+    category VARCHAR(50),
+    mom_calf VARCHAR(50),
+    duplicate VARCHAR(10)
+);
 
-DROP TABLE IF EXISTS `category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `category` (
-  `category_id` int NOT NULL AUTO_INCREMENT,
-  `category_name` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+CREATE TABLE Location (
+    location_id INT AUTO_INCREMENT PRIMARY KEY,
+    latitude DECIMAL(10, 6),
+    longitude DECIMAL(10, 6)
+);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+CREATE TABLE Category (
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(50)
+);
 
--- Dump completed on 2024-04-22 19:51:27
+CREATE TABLE Certainty (
+    certainty_id INT AUTO_INCREMENT PRIMARY KEY,
+    certainty_level VARCHAR(50)
+);
+
+CREATE TABLE GroupSize (
+    group_size_id INT AUTO_INCREMENT PRIMARY KEY,
+    group_size_value INT
+);
