e8688eb9ae84d0e08eb3327313bf98a93f716158
Layan Akhras <lakhras1@umbc.edu>
2024-04-26 18:49:08 -0400

    fixed filtering by year and updated possible options for each filter

--- whale_sightings_app.py
+++ whale_sightings_app.py
@@ -33,6 +33,7 @@ def execute_query(connection, query):
 
 # Function to insert data into the database
 def insert_sightings_data(connection, df):
+   print("Loading data...")
    cursor = connection.cursor()
    for index, row in df.iterrows():
        query = """
@@ -52,6 +53,7 @@ def insert_sightings_data(connection, df):
        cursor.execute(query, values)
        connection.commit()
    cursor.close()
+   print("Loading complete")
 
 
 # Function to filter by certainty
@@ -99,6 +101,20 @@ def filter_by_group_size(connection, group_size):
        print("No results found.")
 
 
+def filter_by_year(connection, year):
+   query = f"""
+   SELECT *
+   FROM Sightings
+   WHERE YEAR(STR_TO_DATE(sighting_date, '%d-%b-%y')) = {year}
+   """
+   result = execute_query(connection, query)
+   if result:
+       for row in result:
+           print(row)
+   else:
+       print("No results found.")
+
+
 # Main function
 def main():
    df = pd.read_csv("C:/WHALE SIGHTINGS/WHALE SIGHTINGS DATASET.csv")
@@ -108,23 +124,33 @@ def main():
 
        # Text-based user interface
        while True:
-           print("\n=== Whale Sightings Menu ===")
+           print("\n=== Whale Watcher ===")
            print("1. Filter by Certainty")
            print("2. Filter by Category")
            print("3. Filter by Group Size")
-           print("4. Exit")
+           print("4. Filter by Year")
+           print("5. Exit")
            choice = input("Enter your choice: ")
 
            if choice == "1":
+               print("Certainties: [Definite, Probable]")
                certainty = input("Enter certainty: ")
                filter_by_certainty(connection, certainty)
            elif choice == "2":
-               category = input("Enter category: ")
+               print("Categories: ")
+               print("[US Coast Guard, Dedicated Eg Aerial, Opportunistic, Commercial vessel, Fishing Vessel,")
+               print("Volunteer Sighting Network, Dedicated Eg Shipboard, Unknown, Whale watch, Acoustic]")
+               category = input("Enter category:")
                filter_by_category(connection, category)
            elif choice == "3":
+               print("Group sizes: [1-100]")
                group_size = int(input("Enter group size: "))
                filter_by_group_size(connection, group_size)
            elif choice == "4":
+               print("Years: [2002-2018]")
+               year = int(input("Enter year: "))
+               filter_by_year(connection, year)
+           elif choice == "5":
                print("Exiting the program.")
                break
            else:
